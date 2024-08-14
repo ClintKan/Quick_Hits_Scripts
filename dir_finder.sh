ff3b163bd00f194f258d03793ac6fe658f1f2e49
Clinton Kanyali <clintkanyali@gmail.com>
2024-08-13 23:32:46 -0400

    Update dir_finder.sh

--- dir_finder.sh
+++ dir_finder.sh
@@ -2,7 +2,7 @@
 
 #This is a script meant to find a directory on the current folder and return the path.
 
-echo "Enter the folder you're looking for" #Prompt to input the name of the file/folder to be looked for
+echo "Enter the file/folder name you're looking for" #Prompt to input the name of the file/folder to be looked for
 read name_find #Input of the name
 
 finder_que=$(ls -al | grep "$name_find") #Listing the contents and then grep-ing the name of the file only and filing them
