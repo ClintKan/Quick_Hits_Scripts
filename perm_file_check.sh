944b8daf20df9be185d3db3b99b4174b1658016c
Clinton Kanyali <clintkanyali@gmail.com>
2024-08-14 18:42:56 -0400

    Update perm_file_check.sh

--- perm_file_check.sh
+++ perm_file_check.sh
@@ -5,5 +5,25 @@
 echo "What is the name of the file?"
 read file_name
 
-echo $(ls -al $file_name |  awk '{print $1}')
+echo $(ls -al $file_name | awk '{print $1}')
 
+if [ -r $file_name ]
+ then
+   echo "The file" $file_name "is readable".
+ else
+   echo "The file" $file_name "is not readable"
+fi
+
+if [ -w $file_name ]
+ then
+   echo "The file" $file_name "is readable/editable".
+ else
+   echo "The file" $file_name "is not writable/editable"
+fi
+
+if [ -x $file_name ]
+ then
+   echo "The file" $file_name "is executable".
+ else
+   echo "The file" $file_name "is not executable"
+fi
