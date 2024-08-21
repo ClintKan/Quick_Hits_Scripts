dda22258e7e30a51e428ad03484f5a28f76c8d3c
Clinton Kanyali <clintkanyali@gmail.com>
2024-08-21 13:35:07 -0400

    Update directory_monitor.sh

--- directory_monitor.sh
+++ directory_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# A script finding helping monitor a dircetory if interested.
+# A script helping monitor a file/dircetory if interested.
 # It is to find the space it occupies and if it exceeds 100MB to notify the user.
 
 #asking the user if they want to monitor a file/directory
@@ -14,7 +14,7 @@ if [ "$usr_inpt" == "yes" ]; then
   read filedir_input
 
     if [ -d "$filedir_input"  ]; then #checking if the file/directory exists
-        filedir_size=$(du -h $filedir_input | tail -n 1 | tail -n 1 | awk '{print$1}') #extracting only the size of the file/directory from the output 
+        filedir_size=$(du $filedir_input | tail -n 1 | awk '{print$1}') #extracting only the size of the file/directory from the output 
         if [ $filedir_size -gt 102400 ]; then #checking if the figure-size is greater than 100MB
           echo $filedir_input "exceeds 100MB"
         else
