3e6f0e2f6cf206e9073bd1cd52d7a944fb9aecdd
Clinton Kanyali <clintkanyali@gmail.com>
2024-08-29 15:02:36 -0400

    Update count_word.sh

--- count_word.sh
+++ count_word.sh
@@ -10,11 +10,12 @@ read word_find
 echo "Enter the absolute path of to the file that has the word you want find"
 read source_filepath
 
-if [-f $source_filepath]; then
+if [ -f $source_filepath ]; then
     #grep searches for the word in the path provided
     #flags; -w for whole word and -o for only the word itself
     word_apps=$(grep -wo "$word_find" "$source_filepath" | wc -l | sed 's/ //g') 
-    
-    echo "The word '$word_find' shows up $word_apps times in the file $source_filepath."
+
+    echo "The word $word_find shows up $word_apps times in the file $source_filepath."
 else
     echo "The file $source_filepath doesn't exist."
+fi
