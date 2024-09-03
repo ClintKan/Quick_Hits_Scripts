092f32375ac5a2518f7bf04753b4e79c7afa28e5
Clinton Kanyali <clintkanyali@gmail.com>
2024-09-03 18:48:30 -0400

    Update files_dir_counter.py

--- files_dir_counter.py
+++ files_dir_counter.py
@@ -2,11 +2,11 @@ import os #importing the 'os' module that deals with the operating system
 
 print("A script that counts the number of files & directories in a specified directory")
 
-files_count = 0
+files_count = 0 #
 dirs_count = 0
 
 dir = input ("Which directory do you want to do the search in?\n")
 
 all_files_dirs = len(os.listdir(dir))
 
-print(all_files_dirs)
+print(f"There is a total of {all_files_dirs}")
