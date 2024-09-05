9de5c77e7f48f2160ccc620c71849e6f1dcbde0c
Clinton Kanyali <clintkanyali@gmail.com>
2024-09-05 18:39:48 -0400

    Update uptime_process_calc.py

--- uptime_process_calc.py
+++ uptime_process_calc.py
@@ -1,9 +1,11 @@
 import math
 
-def uptime_percentage (total_runtime, total_downtime):
-	input("Alright I will calculate how long your service was down for your process.\n")
-	percentage = ((total_runtime / total_downtime) * 100)
-	answer = round(percentage, 2)
+def uptime_percentage(total_runtime, total_downtime):
+	print("Alright I will calculate how long your service was down for your process.\n")
+	percentage = ((total_runtime - total_downtime)/total_runtime * 100)
+	return round(percentage, 2)
 	
+total_runtime = float(input("Punch in the total runtime of the process; "))
+total_downtime = float(input("Punch in the downtime of the process; "))
 
-print(f"The uptime of your process is {answer}")
+print(f"The uptime of your process is {uptime_percentage(total_runtime, total_downtime)}%")
