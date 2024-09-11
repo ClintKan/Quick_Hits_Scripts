551b5bfaf7238083408be14ff87debe14ada951d
Clinton Kanyali <clintkanyali@gmail.com>
2024-09-11 14:16:43 -0400

    Update log_comber.sh

--- log_comber.sh
+++ log_comber.sh
@@ -14,10 +14,10 @@ cat log_review.txt | awk -F '"' '{print $2}' > log_review2.txt #print the 2nd en
 
 cat log_review2.txt | awk '{print $1}' > log_review3.txt  #print the 1st entry-string of each line of the file log_review2.txt into log_review3.txt
 
-tr -s ' ' '\n' < log_review3.txt | sort | uniq -c  > temp_logcomber.txt #organize the strings in the files for sorting and then counting uniqueness 
+cat log_review3.txt |sort -nr | uniq -c | sort -nr > log_comber.txt #temp_logcomber.txt
 
-sort -nr temp_logcomber.txt > log_comber.txt #sort the contents in the temp_logcomber.txt with the one with the highest count on top
+#tr -s ' ' '\n' < log_review3.txt | sort | uniq -c  > temp_logcomber.txt #organize the strings in the files for sorting and then counting uniqueness 
+#sort -nr temp_logcomber.txt > log_comber.txt #sort the contents in the temp_logcomber.txt with the one with the highest count on top
 
-rm log_review.txt && rm log_review2.txt && rm log_review3.txt && rm temp_logcomber.txt # clear out the temp files
-
+rm log_review.txt && rm log_review2.txt && rm log_review3.txt # #rm temp_logcomber.txt to clear out the temp files
 echo "The sorted file named log_comber.txt has been created in the same directory you currently in. You may open it now."
