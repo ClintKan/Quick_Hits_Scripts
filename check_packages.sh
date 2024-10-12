e9fa39d86d26e873ba64e3ab1bdb4ff0a63079da
Home <home@Homes-MBP.lan>
2024-10-12 01:36:20 -0400

    Updating & Adding

--- check_packages.sh
+++ check_packages.sh
@@ -7,23 +7,24 @@ echo "....(systemd, htop, git, python3, curl, nmap, apt)"
 pakg_list=("systemd" "htop" "git" "python3" "curl" "nmap" "apt")
 
 for pakg in ${pakg_list[@]}; do
-     if dpkg -l $pakg; then
-          pakg_vrsn=$(dpkg -l $pakg | awk 'NR==6 {print $3}')
+     if dpkg -l $pakg &> /dev/null; then
+          pakg_vrsn=$(dpkg -l $pakg 2>/dev/null | awk 'NR==6 {print $3}')
           echo "'$pakg' is installed, version $pakg_vrsn"
      else
           echo "'$pakg' is not installed. Would you like to install it? (Yes/no)"
           read usr_inpt # this | tr [:upper:][:lower:] - can be added to this line
           #OR
-          usr_inpt=${usr_input,,}
+          usr_inpt=${usr_inpt,,}
 
-          if [ $usr_inpt == "yes" ]
+          if [ "$usr_inpt" == "yes" ]; then
                echo "Got it! Installing '$pakg'"
                sudo apt install $pakg -y
-          elif [ $usr_inpt == "no" ]
+          elif [ "$usr_inpt" == "no" ]; then
                echo "Got it! Not installing '$pakg'"
           else
                echo "Wrong input - try again and enter either yes or no."
           fi
+     fi
 
 done
 
